6f4ad848fbfd322f1f1405be0c400bf31b7ecec9
kingvern <kingvern@foxmail.com>
2017-09-22 19:10:08 +0800

    小红条

---
+++ app/src/main/java/com/example/kingvern/miniweather/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kingvern.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kingvern on 9/21/17.
+ */
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+
+    }
+}
